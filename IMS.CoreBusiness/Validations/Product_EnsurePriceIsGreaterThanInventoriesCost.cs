8b6d1a2c5a12955241ea364ba0f6291c17b35482
Kevin Wegrowski <kevinw778@gmail.com>
2022-09-13 16:05:19 -0400

    Added custom Validation to Product
    Price set for a Product can no longer be less than the sum of its parts.

---
+++ IMS.CoreBusiness/Validations/Product_EnsurePriceIsGreaterThanInventoriesCost.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMS.CoreBusiness.Validations
+{
+    public class Product_EnsurePriceIsGreaterThanInventoriesCost : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var product = validationContext.ObjectInstance as Product;
+            if (product != null)
+            {
+                if (!ValidatePricing(product))
+                {
+                    return new ValidationResult($"The product's price is less than the inventories cost: {TotalInventoryCost(product).ToString("c")}!",
+                        new List<string>() { validationContext.MemberName });
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+
+        private double TotalInventoryCost(Product product)
+        {
+            if (product == null || product.ProductInventories == null)
+            {
+                return 0;
+            }
+
+            return product.ProductInventories.Sum(x => x.Inventory?.Price * x.InventoryQuantity ?? 0);
+        }
+
+        private bool ValidatePricing(Product product)
+        {
+            if (product.ProductInventories == null || product.ProductInventories.Count <= 0)
+            {
+                return true;
+            }
+
+            // Cost of all parts exceeds price set for product - this should not happen since you'd lose money!
+            if (TotalInventoryCost(product) > product.Price)
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
