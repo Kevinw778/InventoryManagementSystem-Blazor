99f1c51bca9bbd29c2f545f2dc9dfe9301071a1f
Kevin Wegrowski <kevinw778@gmail.com>
2022-09-14 00:36:24 -0400

    Fixed Task vs void

--- IMS.UseCases/PluginInterfaces/IInventoryTransactionRepository.cs
+++ IMS.UseCases/PluginInterfaces/IInventoryTransactionRepository.cs
@@ -4,6 +4,6 @@ namespace IMS.UseCases.PluginInterfaces
 {
     public interface IInventoryTransactionRepository
     {
-        Task PurchaseAsync(string poNumber, Inventory inventory, int quantity, string doneBy, double price);
+        void PurchaseAsync(string poNumber, Inventory inventory, int quantity, string doneBy, double price);
     }
 }
