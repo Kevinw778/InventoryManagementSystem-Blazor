949f5c9cdbab71f4d3bab4a77e039a476a5a5331
Kevin Wegrowski <kevinw778@gmail.com>
2022-09-15 21:48:21 -0400

    Implemented Selling Product + made AutoCompleteComponent's search function asynchronous.

--- IMS.WebApp/Program.cs
+++ IMS.WebApp/Program.cs
@@ -38,6 +38,7 @@ builder.Services.AddTransient<IUpdateProductUseCase, UpdateProductUseCase>();
 // Activities
 builder.Services.AddTransient<IPurchaseInventoryUseCase, PurchaseInventoryUseCase>();
 builder.Services.AddTransient<IProduceProductUseCase, ProduceProductUseCase>();
+builder.Services.AddTransient<ISellProductUseCase, SellProductUseCase>();
 
 
 
---
+++ IMS.WebApp/ViewModels/Validations/Sell_EnsureEnoughProductQuantity.cs
@@ -0,0 +1,29 @@
+﻿using IMS.CoreBusiness;
+using System.ComponentModel.DataAnnotations;
+
+namespace IMS.WebApp.ViewModels.Validations
+{
+    public class Sell_EnsureEnoughProductQuantity : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            var sellViewModel = validationContext.ObjectInstance as SellViewModel;
+            if (sellViewModel != null)
+            {
+                if (sellViewModel.Product != null)
+                {
+                    if (sellViewModel.Product != null)
+                    {
+                        if (sellViewModel.Product.Quantity < sellViewModel.QuantityToSell)
+                        {
+                            return new ValidationResult($"There isn't enough product ({sellViewModel.Product.Quantity}) in the warehouse",
+                                new[] { validationContext.MemberName });
+                        }
+                    }
+                }
+            }
+
+            return ValidationResult.Success;
+        }
+    }
+}
